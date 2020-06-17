7dded94edc33db9678f72b65d9865d82ce4aa48a
Sindre Wilting <sindrewilting@gmail.com>
2020-06-17 15:32:18 +0200

    rudiments

---
+++ Source/rudiments/CascadingBuild.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+export class CascadingBuild {
+    static pusher = 'dolittle-build';
+    static message = '[Cascading release]';
+}
